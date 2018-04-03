ecd07aba2a5371443b73bb49a08babf7ad8eddec
shakram02 <ahmedhamdyau@gmail.com>
2018-04-03 12:07:08 +0200

    convert analog events

--- src/main/kotlin/control/IOEventConverter.kt
+++ src/main/kotlin/control/IOEventConverter.kt
@@ -7,17 +7,26 @@ import java.lang.IllegalArgumentException
 class IOEventConverter {
     private val pinConverter = HashMap<Byte, Byte>()
 
+    companion object {
+        private const val ADC_MAX = 1023L
+        private const val PWM_MAX = 255L
+
+        fun mapToPwm(x: Long): Long {
+            return (x * PWM_MAX) / ADC_MAX
+        }
+    }
+
     fun convert(e: IOEvent): ConvertedEvent {
         val pin = e.pin
 
-        // TODO: match pin numbers on different types
+        // TODO: match pin numbers on different board types
         var convertedPin: Byte = pin.index
 
 
         if (pinConverter.containsKey(convertedPin)) {
             convertedPin = pinConverter[convertedPin] as Byte
         } else {
-            throw IllegalArgumentException("pin ${pin.index} is not mapped to any pins")
+            throw IllegalArgumentException("pin ${pin.index} is not mapped to any output pins")
         }
 
         return convertWithMapping(e, convertedPin)
@@ -26,14 +35,22 @@ class IOEventConverter {
 
     private fun convertWithMapping(e: IOEvent, outPin: Byte): ConvertedEvent {
         val pin = e.pin
+        var convertedPinValue = pin.value
 
         val outMode: PinMode = when (e.pin.mode) {
             Pin.Mode.INPUT -> PinMode.OUTPUT
-            Pin.Mode.ANALOG -> PinMode.PWM
+            Pin.Mode.ANALOG -> {
+                // convert 10-bits analog input to 8-bits PWM output
+                // NOTE we can't set pin.value as this is translated as an
+                // intention to write to input pin, that's why convertedPinValue exists
+                convertedPinValue = mapToPwm(pin.value)
+
+                PinMode.PWM
+            }
             else -> throw IllegalStateException("${e.pin.mode} can't be converted")
         }
 
-        return ConvertedEvent(outPin, outMode, pin.value, e.timestamp)
+        return ConvertedEvent(outPin, outMode, convertedPinValue, e.timestamp)
     }
 
     fun addPinMapping(myPin: Byte, theirPin: Byte) {
@@ -53,6 +70,7 @@ enum class PinMode {
                 "INPUT" -> INPUT
                 "ANALOG" -> ANALOG
                 "OUTPUT" -> OUTPUT
+                "PWM" -> PWM
                 else -> throw IllegalArgumentException("Invalid Enum Value $str")
             }
         }
@@ -80,7 +98,7 @@ data class ConvertedEvent(val pinNumber: Byte, val mode: PinMode, val value: Lon
     }
 
     companion object {
-        fun deserialize(serialized: ByteArray): ConvertedEvent? {
+        fun deserialize(serialized: ByteArray): ConvertedEvent {
             val string = String(serialized)
             val values = string.split("-")
 
